aeb561491fb8c3933d755db7cb56232f1298e663
Canaga81 <Canagamemmedov@gmail.com>
2023-08-30 20:14:53 +0400

    Animasyonlu Ilerleme Addimlari

---
+++ Project_2/app.js
@@ -0,0 +1,60 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+let currentActive = 1;
+
+nextBtn.addEventListener("click", () => {
+
+    currentActive++;
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+
+});
+
+prevBtn.addEventListener("click", () => {
+    
+    currentActive--;
+
+    if(currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+
+});
+
+const update = () => {
+
+    circles.forEach((circle, index) => {
+
+        if(index < currentActive) {
+            circle.classList.add("active");
+        }
+        else {
+            circle.classList.remove("active");
+        }
+        
+    });
+
+    const actives = document.querySelectorAll(".active");
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
+
+    if(currentActive === 1) {
+        prevBtn.disabled = true
+    }
+    else if(currentActive === circles.length) {
+        nextBtn.disabled = true;
+    }
+    else {
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+    }
+
+}
