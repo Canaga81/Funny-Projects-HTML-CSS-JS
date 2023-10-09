45c7749555b24bdc6574017034d56a4de9f883e6
Canaga81 <Canagamemmedov@gmail.com>
2023-10-09 11:10:14 +0400

    CRUD Finish

--- Project_29/app.js
+++ Project_29/app.js
@@ -54,7 +54,7 @@ function Create () {
     const storage = JSON.parse( localStorage.getItem( "userList" ) );
     let inputText = document.getElementById( "name" ).value;
 
-    if ( inputText == "" ) {
+    if ( inputText.trim() == "" ) {
         alert( "Write a name" );
     }
     else {
@@ -70,9 +70,99 @@ function Create () {
             userList = JSON.parse( localStorage.getItem( "userList" ) );
             userList.push( inputText );
             localStorage.setItem( "userList", JSON.stringify( userList ) );
-            
+
         }
 
+    }
+
+}
+
+function Delete ( item ) {
+
+    let deleteUsers = JSON.parse( localStorage.getItem( "userList" ) );
+    deleteUsers.splice( item, 1 );
+    localStorage.setItem( "userList", JSON.stringify( deleteUsers ) );
+    Read();
+
+}
+
+function Edit ( item ) {
+
+    let editUsers = JSON.parse( localStorage.getItem( "userList" ) );
+
+    nameListDom.innerHTML = "";
+
+    for ( let i = 0; i < editUsers.length; i++ ) {
+        if ( i === item ) {
+            nameListDom.innerHTML += `
+                <div class="user-item">
+
+                    <div>
+                        <p>
+                            <i class="fas fa-user"></i>
+                            <span>User :</span> ${ editUsers[i] }
+                        </p>
+                        <input autocomplete="off" type="text" placeholder="${ editUsers[i] }" id="newName">
+                    </div>
+
+                    <div class="buttons">
+
+                        <button class="success" onclick="Update(${ i })">
+                            <i class="fas fa-edit"></i>
+                            Update
+                        </button>
+
+                        <button class="warning" onclick="Read()">
+                            <i class="fas fa-trash"></i>
+                            Cancel
+                        </button>
+
+                    </div>
+
+                </div>
+            `;
+        }
+        else {
+            nameListDom.innerHTML += `
+                    <div class="user-item">
+
+                        <p>
+                            <i class="fas fa-user"></i>
+                            <span>User :</span> ${ getUserList[i] }
+                        </p>
+
+                        <div class="buttons">
+
+                            <button class="primary" onclick="Edit(${ i })">
+                                <i class="fas fa-edit"></i>
+                                Edit
+                            </button>
+
+                            <button class="danger" onclick="Delete(${ i })">
+                                <i class="fas fa-trash"></i>
+                                Delete
+                            </button>
+
+                        </div>
+                    
+                    </div>
+                `;
+        }
+    }
+
+}
+
+function Update ( item ) {
+
+    let updateUsers = JSON.parse( localStorage.getItem( "userList" ) );
+    updateUsers[item] = document.getElementById( 'newName' ).value;
+
+    if ( updateUsers[item] === "" ) {
+        alert( "Write a new Name" );
+    }
+    else {
+        localStorage.setItem( "userList", JSON.stringify( updateUsers ) );
+        Read();
     }
 
 }
@@ -84,11 +174,6 @@ formEl.addEventListener( "submit", ( e ) => {
     Read();
     formEl.reset();
 } );
-
-
-// function Edit ( item ) {
-//     console.log(item);
-// }
 
 document.addEventListener( "DOMContentLoaded", () => {
     Read();
